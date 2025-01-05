78484c9b0d2aa3ab4279cceb40166142046598bd
Krishna Upadhyay <krishna.upadhyay911@gmail.com>
2025-01-05 13:03:22 +0530

    strm reachh .

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -29,7 +29,7 @@ export default function Gallery() {
       <div className="w-full h-full relative top-[120px] flex flex-col items-center">
         <div className="w-full flex flex-wrap justify-center gap-6 md:gap-12 mt-12">
             {images.map((image, index) => (
-                <div className="w-[310px] h-[310px] rounded-2xl overflow-hidden" key={index}>
+                <div className="w-[320px] rounded-2xl overflow-hidden" key={index}>
                     <img src={image.src} alt="Gentle Hands Images" className="w-full h-full object-cover" />
                 </div>
             ))}
